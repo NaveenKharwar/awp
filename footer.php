571655654fa1a03d0dad91fed1785204df896392
Naveen Kharwar <naveenkharwar21@protonmail.com>
2021-04-12 13:43:12 +0530

    Improve typo from footer

--- footer.php
+++ footer.php
@@ -35,7 +35,7 @@
 			<span class="sep"> | </span>
 			<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'agilitywp' ), 'Agility WP', '<a href="https://lucenthemes.com/">Luncent Themes</a>' );
+				printf( esc_html__( 'Themed %s.', 'agilitywp' ), '<a href="https://lucenthemes.com/">Luncent Themes</a>' );
 			?>
 		</div><!-- .site-info -->
 	</div>
