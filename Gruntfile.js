b012b65df44e28f79e55cb815f25d3288016ed6f
Naveen Kharwar <naveenkharwar21@protonmail.com>
2022-12-06 17:26:21 +0530

    improve grunt command and indent codes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,146 +1,154 @@
-module.exports = function (grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      dev: {
-        options: {
-          style: "expanded",
-          loadPath: 'node_modules/bootstrap/scss'
-        },
-        files: {
-          'assets/css/theme.css': 'resources/sass/theme.scss',
-          'style.css': 'resources/sass/style.scss',
-          'assets/css/fonts.css': 'resources/sass/fonts.scss',
-          'assets/css/style-editor.css':
+const gruntConfig = function( grunt ) {
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+		sass: {
+			dev: {
+				options: {
+					style: 'expanded',
+					loadPath: 'node_modules/bootstrap/scss',
+				},
+				files: {
+					'assets/css/theme.css': 'resources/sass/theme.scss',
+					'style.css': 'resources/sass/style.scss',
+					'assets/css/fonts.css': 'resources/sass/fonts.scss',
+					'assets/css/style-editor.css':
             'resources/sass/editor/style-editor.scss',
-        },
-      },
-      dist: {
-        options: {
-          style: 'expanded',
-          style: "compressed",
-          loadPath: 'node_modules/bootstrap/scss'
-        },
-        files: {
-          'assets/css/theme.css': 'resources/sass/theme.scss',
-          'style.css': 'resources/sass/style.scss',
-          'assets/css/fonts.css': 'resources/sass/fonts.scss',
-          'assets/css/style-editor.css':
+				},
+			},
+			dist: {
+				options: {
+					style: 'compressed',
+					loadPath: 'node_modules/bootstrap/scss',
+				},
+				files: {
+					'assets/css/theme.css': 'resources/sass/theme.scss',
+					'style.css': 'resources/sass/style.scss',
+					'assets/css/fonts.css': 'resources/sass/fonts.scss',
+					'assets/css/style-editor.css':
             'resources/sass/editor/style-editor.scss',
-        },
-      },
-    },
-    cssmin: {
-      build: {
-        expand: true,
-        src: ['assets/css/theme.css', 'assets/css/fonts.css'],
-        ext: '.min.css',
-      },
-    },
-    copy: {
-      font: {
-        expand: true,
-        flatten: true,
-        src: [
-          'resources/fonts/Catamaran/**',
-          'resources/fonts/Quicksand/**',
-        ],
-        dest: 'assets/fonts/',
-        filter: 'isFile'
-      },
-      bootstrap_script: {
-        expand: true,
-        flatten: true,
-        src: "node_modules/bootstrap/dist/js/bootstrap.min.js",
-        dest: "assets/js/",
-        filter: "isFile"
-      },
-      bootstrap_style: {
-        expand: true,
-        flatten: true,
-        src: "node_modules/bootstrap/scss/bootstrap.scss",
-        dest: "resources/sass/framework/",
-        filter: "isFile"
-      },
-      build: {
-        src: [
-          '**',
-          '!node_modules/**',
-          '!vendor/**',
-          '!Gruntfile.js',
-          '!package.json',
-          '!package-lock.json',
-          '!resources/**',
-          '!composer.json',
-          '!composer.lock',
-          '!phpcs.xml.dist',
-          '!README.md',
-          '!CHANGELOG.md',
-          '!style.css.map',
-          '!assets/css/**.css.map',
-          '!wpcs',
-          // "!theme.css.map",
-          // "!style-editor.css.map",
-          '!woocommerce.css.map',
-        ],
-        dest: 'build/',
-      },
-    },
-    compress: {
-      build: {
-        options: {
-          archive: "<%= pkg.name %> - <%= pkg.version %> on <%= grunt.template.today('yyyy-mm-dd HH:mm:ss') %> .zip",
-        },
-        files: [
-          {
-            expand: true,
-            cwd: 'build/',
-            src: ['**/*'],
-          },
-        ],
-      },
-    },
-    clean: {
-      dist: {
-        src: ['build', '**/**.zip'],
-      },
-    },
-    version: {
-      stylesheet: {
-        options: {
-          prefix: 'Version\\:\\s+',
-        },
-        src: 'style.css',
-      },
-      functions: {
-        options: {
-          prefix: "AGILITYWP_VERSION', '",
-        },
-        src: 'functions.php',
-      },
-    },
-    watch: {
-      css: {
-        files: '**/*.scss',
-        tasks: ['sass:dev', 'cssmin', 'copy', 'version'],
-        options: {
-          spawn: false,
-        },
-      },
-    },
-  });
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-version');
-  // Watch Task
-  grunt.registerTask('default', ['watch']);
-  // build task
-  grunt.registerTask('build', ['sass:dev', 'cssmin', 'copy', 'version']);
-  // Release Task
-  grunt.registerTask( 'prod', [ 'sass:dist', 'cssmin',, 'version', 'copy', 'compress', ] );
+				},
+			},
+		},
+		cssmin: {
+			build: {
+				expand: true,
+				src: [ 'assets/css/theme.css', 'assets/css/fonts.css' ],
+				ext: '.min.css',
+			},
+		},
+		copy: {
+			font: {
+				expand: true,
+				flatten: true,
+				src: [
+					'resources/fonts/Catamaran/**',
+					'resources/fonts/Quicksand/**',
+				],
+				dest: 'assets/fonts/',
+				filter: 'isFile',
+			},
+			bootstrap_script: {
+				expand: true,
+				flatten: true,
+				src: 'node_modules/bootstrap/dist/js/bootstrap.min.js',
+				dest: 'assets/js/',
+				filter: 'isFile',
+			},
+			bootstrap_style: {
+				expand: true,
+				flatten: true,
+				src: 'node_modules/bootstrap/scss/bootstrap.scss',
+				dest: 'resources/sass/framework/',
+				filter: 'isFile',
+			},
+			build: {
+				src: [
+					'**',
+					'!node_modules/**',
+					'!vendor/**',
+					'!Gruntfile.js',
+					'!package.json',
+					'!package-lock.json',
+					'!resources/**',
+					'!composer.json',
+					'!composer.lock',
+					'!phpcs.xml.dist',
+					'!README.md',
+					'!CHANGELOG.md',
+					'!style.css.map',
+					'!assets/css/**.css.map',
+					'!wpcs',
+					// "!theme.css.map",
+					// "!style-editor.css.map",
+					'!woocommerce.css.map',
+				],
+				dest: 'build/',
+			},
+		},
+		uglify: {
+			theme_js: {
+				files: {
+					'assets/js/customizer.min.js': [ 'resources/js/customizer.js' ],
+				},
+			},
+		},
+		compress: {
+			build: {
+				options: {
+					archive: "<%= pkg.name %> - <%= pkg.version %> on <%= grunt.template.today('yyyy-mm-dd HH:mm:ss') %> .zip",
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'build/',
+						src: [ '**/*' ],
+					},
+				],
+			},
+		},
+		clean: {
+			dist: {
+				src: [ 'build', '**/**.zip' ],
+			},
+		},
+		version: {
+			stylesheet: {
+				options: {
+					prefix: 'Version\\:\\s+',
+				},
+				src: 'style.css',
+			},
+			functions: {
+				options: {
+					prefix: "AGILITYWP_VERSION', '",
+				},
+				src: 'functions.php',
+			},
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: [ 'sass:dev', 'cssmin', 'uglify', 'copy', 'version' ],
+				options: {
+					spawn: false,
+				},
+			},
+		},
+	} );
+	grunt.loadNpmTasks( 'grunt-contrib-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-contrib-compress' );
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-version' );
+	// Watch Task
+	grunt.registerTask( 'default', [ 'watch' ] );
+	// build task
+	grunt.registerTask( 'build', [ 'sass:dev', 'cssmin', 'uglify', 'copy', 'version' ] );
+	// Release Task
+	grunt.registerTask( 'prod', [ 'sass:dist', 'cssmin', 'uglify', 'version', 'copy', 'compress' ] );
 };
+
+module.exports = gruntConfig;
