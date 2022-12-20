6df1625283d4331f72261dea7c92bb4c2847faa6
Naveen Kharwar <naveenkharwar21@protonmail.com>
2022-12-20 19:01:23 +0530

    Finally adding some code to this file after months of procrastination. The world may never know the struggles I faced while typing these measly lines. #firstworldproblems

---
+++ template-parts/content-pagebuilder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying the content from the page builder.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package AgilityWP
+ * @since 0.0.3
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+the_content();
